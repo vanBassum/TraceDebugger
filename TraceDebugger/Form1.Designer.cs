dc043b878ea97281c242cfaba031fdec252c1349
Bas Visscher <bas@koolecontrols.nl>
2022-06-23 12:22:49 +0200

    Added project

---
+++ TraceDebugger/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace TraceDebugger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.scopeView1 = new FRMLib.Scope.Controls.ScopeView();
+            this.markerView1 = new FRMLib.Scope.Controls.MarkerView();
+            this.traceView1 = new FRMLib.Scope.Controls.TraceView();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
+            this.splitContainer3.Panel1.SuspendLayout();
+            this.splitContainer3.Panel2.SuspendLayout();
+            this.splitContainer3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(359, 445);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Console";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.traceView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(359, 445);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Markers";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(714, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // splitContainer3
+            // 
+            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer3.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+            this.splitContainer3.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer3.Name = "splitContainer3";
+            this.splitContainer3.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer3.Panel1
+            // 
+            this.splitContainer3.Panel1.Controls.Add(this.scopeView1);
+            this.splitContainer3.Panel1.Controls.Add(this.menuStrip1);
+            // 
+            // splitContainer3.Panel2
+            // 
+            this.splitContainer3.Panel2.Controls.Add(this.markerView1);
+            this.splitContainer3.Size = new System.Drawing.Size(714, 471);
+            this.splitContainer3.SplitterDistance = 367;
+            this.splitContainer3.TabIndex = 1;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer3);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tabControl1);
+            this.splitContainer1.Size = new System.Drawing.Size(1085, 471);
+            this.splitContainer1.SplitterDistance = 714;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(367, 471);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // scopeView1
+            // 
+            this.scopeView1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.scopeView1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.scopeView1.DataSource = null;
+            this.scopeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.scopeView1.Location = new System.Drawing.Point(0, 24);
+            this.scopeView1.Name = "scopeView1";
+            this.scopeView1.Size = new System.Drawing.Size(714, 343);
+            this.scopeView1.TabIndex = 2;
+            // 
+            // markerView1
+            // 
+            this.markerView1.DataSource = null;
+            this.markerView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.markerView1.Location = new System.Drawing.Point(0, 0);
+            this.markerView1.Name = "markerView1";
+            this.markerView1.Size = new System.Drawing.Size(714, 100);
+            this.markerView1.TabIndex = 1;
+            // 
+            // traceView1
+            // 
+            this.traceView1.DataSource = null;
+            this.traceView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.traceView1.Location = new System.Drawing.Point(3, 3);
+            this.traceView1.Name = "traceView1";
+            this.traceView1.Size = new System.Drawing.Size(353, 439);
+            this.traceView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1085, 471);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabPage1.ResumeLayout(false);
+            this.splitContainer3.Panel1.ResumeLayout(false);
+            this.splitContainer3.Panel1.PerformLayout();
+            this.splitContainer3.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
+            this.splitContainer3.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.SplitContainer splitContainer3;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private FRMLib.Scope.Controls.TraceView traceView1;
+        private FRMLib.Scope.Controls.ScopeView scopeView1;
+        private FRMLib.Scope.Controls.MarkerView markerView1;
+    }
+}
+
---
+++ TraceDebugger/Form1.cs
@@ -0,0 +1,57 @@
+﻿using FRMLib;
+using FRMLib.Scope;
+using STDLib.Math;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TraceDebugger
+{
+    public partial class Form1 : Form
+    {
+        ScopeController scope;
+        public Form1()
+        {
+            InitializeComponent();
+            menuStrip1.AddMenuItem("File/New", () => scope.ClearData());
+            //menuStrip1.AddMenuItem("File/Open...", OpenFromFileDialog);
+            //menuStrip1.AddMenuItem("File/Save As...", SaveToFileDialog);
+            menuStrip1.AddMenuItem("File/Close", () => this.Close());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            scope = new ScopeController();
+            traceView1.DataSource = scope;
+            scopeView1.DataSource = scope;
+            markerView1.DataSource = scope;
+
+            scope.Settings.DrawScalePosVertical = DrawPosVertical.Right;
+            scope.Settings.DrawScalePosHorizontal = DrawPosHorizontal.Bottom;
+            scope.Settings.HorizontalDivisions = 10;
+            scope.Settings.HorizontalToHumanReadable = (a) => a.ToString("F0") + "s";
+            scope.Settings.HorSnapSize = 0.1f;
+
+            Trace trace = new Trace { Name = "Temp", Offset = 0, Scale = 5, Layer = 9, Unit = "°C", Pen = Palettes.DistinctivePallet[2], DrawStyle = Trace.DrawStyles.Lines, DrawOption = Trace.DrawOptions.ShowScale };
+
+            //string dateString = dt.ToString("dd-MM-yyyy") + "\r\n" + dt.ToString("HH:mm:ss");
+
+
+            scope.Traces.Add(trace);
+
+            trace.Points.Add(new PointD(0, 1));
+            trace.Points.Add(new PointD(1, 2));
+            trace.Points.Add(new PointD(2, 3));
+
+
+
+
+        }
+    }
+}
